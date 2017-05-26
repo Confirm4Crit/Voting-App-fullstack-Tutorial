67525de389fce8e3f71378f42654d26f66a0578e
Miles Robson <miles_robson@yahoo.com>
2017-05-26 16:45:27 -0400

    Sort by votes and formatting cleanup

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,23 @@
-class ProductList extends React.Component{
-    render(){
-        const product = Seed.products[0];
+class ProductList extends React.Component {
+    render() {
+        const products = Seed.products.sort((a, b) => (
+            b.votes - a.votes
+        ));
+        const productComponents = Seed.products.map((product) => (
+            <Product
+                key={'product-' + product.id}
+                id={product.id}
+                title={product.title}
+                description={product.description}
+                url={product.url}
+                votes={product.votes}
+                submitterAvatarUrl={product.submitterAvatarUrl}
+                productImageUrl={product.productImageUrl}
+            />
+        ));
         return (
-            <div className="ui unstackable items">
-                <Product
-                id = {product.id}
-                title = {product.title}
-                descripition = {product.description}
-                url = {product.url}
-                votes = {product.votes}
-                submitterAvatarUrl = {product.submitterAvatarUrl}
-                productImageUrl = {product.productImageUrl}
-                />
+            <div className='ui unstackable items'>
+                {productComponents}
             </div>
         );
     }
@@ -22,12 +28,12 @@ class Product extends React.Component {
         return (
             <div className='item'>
                 <div className='image'>
-                    <img src={this.props.productImageUrl} />
+                    <img src={this.props.productImageUrl}/>
                 </div>
                 <div className='middle aligned content'>
                     <div className='header'>
                         <a>
-                            <i className='large caret up icon' />
+                            <i className='large caret up icon'/>
                         </a>
                         {this.props.votes}
                     </div>
